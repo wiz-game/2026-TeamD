cd8aea7f6666f57ecdffa36212bd3f528e3739e8
Tomoki Yoshida <zeal23428042@fsgcl.jp>
2026-04-06 10:13:30 +0900

    Add : CreateやUpdate関数を追加、変数は親の位置・前方向取得と生成位置設定のためにm_parent と m_trans を追加

--- Project/GameSources/Bubble.cpp
+++ Project/GameSources/Bubble.cpp
@@ -1,2 +1,43 @@
 #include "stdafx.h"
 #include "Project.h"
+
+namespace basecross
+{
+	Bubble::Bubble(const shared_ptr<Stage>& stage, const shared_ptr<GameObject>& parent) :
+		GameObject(stage),
+		m_parent(parent)
+	{
+
+	}
+
+	Bubble::~Bubble()
+	{
+
+	}
+
+	void Bubble::OnCreate()
+	{
+		// 親の情報の取得
+		auto parentLock = m_parent.lock();
+		m_parentForward = parentLock->GetComponent<Transform>()->GetForward();
+		auto parentPos = parentLock->GetComponent<Transform>()->GetPosition();
+		
+		m_trans = GetComponent<Transform>();
+		m_trans->SetPosition(parentPos + (m_parentForward * 1.5f));
+		m_trans->SetScale(Vec3(0.1f));
+
+		auto ptrCol = AddComponent<CollisionSphere>();
+		ptrCol->SetDrawActive(false);
+
+		m_draw = AddComponent<PNTStaticDraw>();
+		m_draw->SetMeshResource(L"DEFAULT_SPHERE");
+
+		AddTag(L"Bubble");
+	}
+
+	void Bubble::OnUpdate()
+	{
+
+	}
+
+}
--- Project/GameSources/Bubble.h
+++ Project/GameSources/Bubble.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "stdafx.h"
+namespace basecross
+{
+	class Bubble : public GameObject
+	{
+		shared_ptr<Transform> m_trans;
+		shared_ptr<PNTStaticDraw> m_draw;
+		weak_ptr<GameObject> m_parent;
+		Vec3 m_parentForward;
+
+	public :
+		Bubble::Bubble(const shared_ptr<Stage>& stage,const shared_ptr<GameObject>& parent);
+		Bubble::~Bubble();
+
+		void OnCreate() override;
+		void OnUpdate() override;
+	};
+}
