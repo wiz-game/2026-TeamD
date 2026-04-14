872765c992350f58d9f5c50fd837a8187c079c56
Takato Kanno <zeal23420052@fsgcl.jp>
2026-04-14 10:26:41 +0900

    Update：GameModeを追加

--- Project/GameSources/GameManager.cpp
+++ Project/GameSources/GameManager.cpp
@@ -1,2 +1,9 @@
 #include "stdafx.h"
 #include "Project.h"
+
+namespace basecross 
+{
+	void GameManager::Initialize() 
+	{
+	}
+}
--- Project/GameSources/GameManager.h
+++ Project/GameSources/GameManager.h
@@ -1,1 +1,40 @@
+/*!
+@file GameManager.h
+@brief ゲームマネージャー 
+@copyright 菅野 皐
+*/
+
 #pragma once
+#include "stdafx.h"
+
+namespace basecross 
+{
+	enum ENUM_GameMode
+	{
+		Play,
+		Menu,
+		Editor
+	};
+
+	class GameManager
+	{
+	private:
+		ENUM_GameMode m_gameMode = ENUM_GameMode::Menu;
+	private:
+		GameManager() {}
+		virtual ~GameManager() {}
+	public:
+		static GameManager& Instance()
+		{
+			static GameManager instance;
+			return instance;
+		}
+
+		void Initialize();
+
+		// アクセサー
+		ENUM_GameMode GetGameMode() const { return m_gameMode; }
+		
+	};
+
+}
