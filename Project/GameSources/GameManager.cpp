583e2af1c9507814a78352e345d83c9eeae36f24
Takato Kanno <zeal23420052@fsgcl.jp>
2026-04-14 17:09:45 +0900

    Update：GameModeの切り替え処理用の関数を追加

--- Project/GameSources/GameManager.cpp
+++ Project/GameSources/GameManager.cpp
@@ -3,7 +3,48 @@
 
 namespace basecross 
 {
-	void GameManager::Initialize() 
+	void GameManager::ExitGameMode(ENUM_GameMode gameMode)
 	{
+		switch (gameMode)
+		{
+		case ENUM_GameMode::Play:
+			break;
+		case ENUM_GameMode::Menu:
+			break;
+		case ENUM_GameMode::Editor:
+			for (auto& gameObject : App::GetApp()->GetScene<Scene>()->GetActiveStage()->GetGameObjectVec())
+				gameObject->SetUpdateActive(true);
+			break;
+		default:
+			break;
+		}
+	}
+
+	void GameManager::EnterGameMode(ENUM_GameMode gameMode)
+	{
+		switch (gameMode)
+		{	
+		case ENUM_GameMode::Play:
+			break;
+		case ENUM_GameMode::Menu:
+			break;
+		case ENUM_GameMode::Editor:
+			for (auto& gameObject : App::GetApp()->GetScene<Scene>()->GetActiveStage()->GetGameObjectVec()) 
+				gameObject->SetUpdateActive(false);
+			break;
+		default:
+			break;
+		}
+	}
+
+	void GameManager::Initialize()
+	{
+	}
+
+	void GameManager::SetGameMode(ENUM_GameMode gameMode)
+	{
+		ExitGameMode(m_gameMode);
+		m_gameMode = gameMode;
+		EnterGameMode(m_gameMode);
 	}
 }
