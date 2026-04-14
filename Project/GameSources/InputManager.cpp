d656fa1e5e6fc8ff570b82b0ffc0081675da8512
Takato Kanno <zeal23420052@fsgcl.jp>
2026-04-14 12:47:05 +0900

    Update：GameMode：Play用の入力処理を追加

--- Project/GameSources/InputManager.cpp
+++ Project/GameSources/InputManager.cpp
@@ -1,2 +1,106 @@
 #include "stdafx.h"
 #include "Project.h"
+
+namespace basecross 
+{
+	void InputManager::Initialize()
+	{
+		m_pad = App::GetApp()->GetInputDevice().GetControlerVec()[0];
+		m_key = App::GetApp()->GetInputDevice().GetKeyState();
+	}
+
+	void InputManager::Update()
+	{
+		switch (GameManager::Instance().GetGameMode())
+		{
+		default:
+			break;
+		case ENUM_GameMode::Play :
+			// プレイヤーの移動
+			if (m_pad.fThumbLX > STACK_DEADZONE_L || m_pad.fThumbLX < -STACK_DEADZONE_L ||
+				m_pad.fThumbLY > STACK_DEADZONE_L || m_pad.fThumbLY < -STACK_DEADZONE_L)
+			{
+				Moves();
+			}
+
+			// 視点移動
+			if (m_pad.fThumbRX > STACK_DEADZONE_R || m_pad.fThumbRX < -STACK_DEADZONE_R ||
+				m_pad.fThumbRY > STACK_DEADZONE_R || m_pad.fThumbRY < -STACK_DEADZONE_R)
+			{
+				MoveCamera();
+			}
+
+			// ジャンプ/戻る
+			if (m_pad.wPressedButtons & XINPUT_GAMEPAD_B)
+			{
+				PressedB();
+			}
+
+			// 決定
+			if (m_pad.wPressedButtons & XINPUT_GAMEPAD_A)
+			{
+				PressedA();
+			}
+
+			// 照準
+			if (m_pad.wButtons & XINPUT_GAMEPAD_LEFT_THUMB)
+			{
+				PushLThumb();
+			}
+
+			// 泡発射
+			if (m_pad.wButtons & XINPUT_GAMEPAD_RIGHT_THUMB)
+			{
+				PushRThumb();
+			}
+
+			// ポーズメニュー
+			if (m_pad.wPressedButtons & XINPUT_GAMEPAD_START)
+			{
+				PressedStart();
+			}
+			break;
+		case ENUM_GameMode::Editor:
+			// 注視点固定視点移動
+			// カメラ位置固定視点移動
+			// オブジェクト選択
+			// オブジェクト削除
+			// 選択解除
+			// 座標移動へ切替
+			// 回転へ切替
+			// サイズへ切替
+			// 生成オブジェクト切替
+			// エディターモード切替
+			break;
+		}
+	}
+
+	void InputManager::Moves()
+	{
+
+	}
+
+	void InputManager::MoveCamera()
+	{
+	}
+
+	void InputManager::PushLThumb()
+	{
+	}
+
+	void InputManager::PushRThumb()
+	{
+	}
+
+	void InputManager::PressedA()
+	{
+	}
+
+	void InputManager::PressedB()
+	{
+	}
+
+	void InputManager::PressedStart()
+	{
+	}
+}
--- Project/GameSources/InputManager.h
+++ Project/GameSources/InputManager.h
@@ -1,1 +1,53 @@
+/*!
+@file InputManager.h
+@brief 入力マネージャー
+@copyright 菅野 皐
+*/
+
 #pragma once
+#include "stdafx.h"
+
+namespace basecross
+{
+	class InputManager
+	{
+	private:
+		CONTROLER_STATE m_pad = CONTROLER_STATE();
+		KEYBOARD_STATE m_key = KEYBOARD_STATE();
+
+		const float STACK_DEADZONE_L = 0.0f;
+		const float STACK_DEADZONE_R = 0.0f;
+
+	private:
+		InputManager() {}
+		virtual ~InputManager() {}
+	public:
+		static InputManager& Instance()
+		{
+			static InputManager instance;
+			return instance;
+		}
+
+		void Initialize();
+		void Update();
+
+		// GameMode：Play用の入力関数
+		// 移動入力
+		void Moves();
+
+		// 視点移動入力
+		void MoveCamera();
+
+		// Push
+		void PushLThumb();
+		void PushRThumb();
+
+		// Pressed
+		void PressedA();
+		void PressedB();
+		void PressedStart();
+
+		// GameMode：Editer用の入力関数
+
+	};
+}
