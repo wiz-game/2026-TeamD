90e02a7a10569d25d958d950353395a7f4c9b1e1
Tomoki Yoshida <zeal23428042@fsgcl.jp>
2026-04-16 15:31:33 +0900

    Add : プレイヤーのメッシュから頂点情報の取得、その位置にViewBubble生成

--- Project/GameSources/FurBubble.cpp
+++ Project/GameSources/FurBubble.cpp
@@ -1,2 +1,40 @@
 #include "stdafx.h"
 #include "Project.h"
+
+namespace basecross
+{
+	FurBubble::FurBubble(const shared_ptr<GameObject>& gameObject, const shared_ptr<Stage>& stage) :
+		Component(gameObject),
+		m_stage(stage)
+	{
+	}
+
+	FurBubble::~FurBubble()
+	{
+	}
+
+	void FurBubble::OnCreate()
+	{
+		m_trans = GetGameObject()->GetComponent<Transform>();
+
+		m_draw = GetGameObject()->GetComponent<PNTStaticDraw>();
+		m_vertices = m_draw->GetMeshResource()->GetNumVertices();
+		m_draw->GetStaticMeshWorldPositions(m_meshPosition);
+		CreateBubble();
+	}
+
+	void FurBubble::OnUpdate()
+	{
+	}
+
+	void FurBubble::OnDraw()
+	{
+
+	}
+
+	void FurBubble::CreateBubble()
+	{
+		m_stage->AddGameObject<ViewBubble>(m_draw, m_vertices, GetGameObject());
+	}
+
+}
--- Project/GameSources/FurBubble.h
+++ Project/GameSources/FurBubble.h
@@ -1,1 +1,25 @@
 #pragma once
+#include "stdafx.h"
+
+namespace basecross
+{
+	class FurBubble : public Component
+	{
+	private:
+		shared_ptr<Transform> m_trans;
+		shared_ptr<PNTStaticDraw> m_draw;
+		shared_ptr<Stage> m_stage;
+		vector<Vec3> m_meshPosition;
+		UINT m_vertices;
+
+	public:
+		FurBubble(const shared_ptr<GameObject>& gameObject,const shared_ptr<Stage>& stage);
+		virtual ~FurBubble();
+
+		virtual void OnCreate();
+		virtual void OnUpdate();
+		virtual void OnDraw();
+
+		void CreateBubble();
+	};
+}
