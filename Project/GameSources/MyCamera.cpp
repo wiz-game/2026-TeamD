bf0c410aea8aff65defc5d3e17308f1b1514464b
Takato Kanno <zeal23420052@fsgcl.jp>
2026-04-21 12:36:49 +0900

    Update：MyCameraの基礎処理を追加、各関数を定義しIsAimingのアクセサを設定

--- Project/GameSources/MyCamera.cpp
+++ Project/GameSources/MyCamera.cpp
@@ -4,5 +4,58 @@
 
 namespace basecross
 {
+	MyCamera::MyCamera()
+	{
+	}
 
+	MyCamera::~MyCamera()
+	{
+	}
+
+	void MyCamera::OnCreate()
+	{
+		Camera::OnCreate();
+	}
+	
+	void MyCamera::OnUpdate()
+	{
+		switch (GameManager::Instance().GetGameMode())
+		{
+		default:
+			break;
+		case ENUM_GameMode::Play:
+			UpdatePlayMode();
+			break;
+		case ENUM_GameMode::Menu:
+			UpdateMenuMode();
+			break;
+		case ENUM_GameMode::Editor:
+			UpdateEditorMode();
+			break;
+		}
+		
+		// CameraクラスのUpdateを最後に呼ぶ
+		Camera::OnUpdate();
+	}
+
+	void MyCamera::PlayModeMove()
+	{
+		// InputManagerからの入力を受け取り、カメラを操作する処理
+		// InputManagerのMoveCamera()から呼び出される想定
+		// 引数は必要に応じ追加してください
+		// Lトリガー（現在でいうXボタン）を押している時の処理はメンバ変数のm_isAimingで切り替えるようにしてください
+	}
+	
+	void MyCamera::UpdatePlayMode()
+	{
+		// プレイヤーに追従する処理
+	}
+	
+	void MyCamera::UpdateMenuMode()
+	{
+	}
+
+	void MyCamera::UpdateEditorMode()
+	{
+	}
 }
