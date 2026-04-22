995d7f5605ccb9d977b481b249b4cadfba01ec6a
Shin Muraoka <zeal24450012@fsgcl.jp>
2026-04-22 10:27:02 +0900

    fix : Playerの位置を直した

--- Project/GameSources/Player.h
+++ Project/GameSources/Player.h
@@ -32,7 +32,7 @@ namespace basecross
 		Player(const std::shared_ptr<Stage>& stage) :
 			GameObject(stage), // ƒXƒe[ƒW‚ğGameObject‚É“n‚·y•K{z
 			m_Velocity(0.0f),
-			m_Position(0.0f, 80.0f, 0.0f),
+			m_Position(0.0f, 60.2f, 0.0f),
 			m_isJumping(false),
 			m_JumpPower(6.0f),
 			m_Gravity(9.8f),
