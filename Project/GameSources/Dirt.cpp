cc30780a4f4a13932451a9a1c510a800a82ca4a0
Tomoki Yoshida <zeal23428042@fsgcl.jp>
2026-05-07 10:39:20 +0900

    Add : OnCreateやUpdate、泡に当たったら消える処理を追加

--- Project/GameSources/Dirt.cpp
+++ Project/GameSources/Dirt.cpp
@@ -1,2 +1,54 @@
 #include "stdafx.h"
 #include "Project.h"
+
+namespace basecross
+{
+	Dirt::Dirt(
+		const shared_ptr<Stage>& StagePtr,
+		const Vec3& Scale,
+		const Quat& Quatition,
+		const Vec3& Position
+	):
+		GameObject(StagePtr),
+		m_nowDeleteCount(0),
+		m_alphaColor(1.0f)
+	{
+		m_scale = Scale;
+		m_quatition = Quatition;
+		m_position = Position;
+	}
+
+	Dirt::~Dirt()
+	{
+
+	}
+
+	void Dirt::OnCreate()
+	{
+		AddTag(L"Dirt");
+
+		m_trans = AddComponent<Transform>();
+		m_trans->SetPosition(m_position);
+		m_trans->SetScale(m_scale);
+		m_trans->SetQuaternion(m_quatition);
+
+		m_draw = AddComponent<PNTStaticDraw>();
+		m_draw->SetMeshResource(L"DEFAULT_CUBE");
+
+		auto col = AddComponent<CollisionObb>();
+		col->SetDrawActive(true);
+		col->SetAfterCollision(AfterCollision::None);
+	}
+
+	void Dirt::OnUpdate()
+	{
+	}
+
+	void Dirt::OnCollisionEnter(shared_ptr<GameObject>& Other)
+	{
+		if (Other->FindTag(L"Bubble"))
+		{
+			GetStage()->RemoveGameObject<Dirt>(GetThis<Dirt>());
+		}
+	}
+}
--- Project/GameSources/Dirt.h
+++ Project/GameSources/Dirt.h
@@ -5,3 +5,33 @@
 
 #pragma once
 #include "stdafx.h"
+
+namespace basecross
+{
+	class Dirt : public GameObject
+	{
+	private:
+		shared_ptr<Transform> m_trans;
+		shared_ptr<PNTStaticDraw> m_draw;
+		Vec3 m_scale;
+		Quat m_quatition;
+		Vec3 m_position;
+		int m_nowDeleteCount;
+		float m_alphaColor;
+
+	public:
+		Dirt(
+			const shared_ptr<Stage>& StagePtr,
+			const Vec3& Scale,
+			const Quat& Quatition,
+			const Vec3& Position
+			);
+		~Dirt();
+
+		virtual void OnCreate()override;
+		virtual void OnUpdate()override;
+
+		void OnCollisionEnter(shared_ptr<GameObject>& Other);
+
+	};
+}
