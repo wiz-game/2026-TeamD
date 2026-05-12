887055f6aad03f9817e7eb6d0f14de8888fe61b6
Tomoki Yoshida <zeal23428042@fsgcl.jp>
2026-05-12 14:46:26 +0900

    Update : 泡の機能を追加できる関数を追加、Playerでこの関数を呼ぶことで泡に機能を追加できる

--- Project/GameSources/Bubble.h
+++ Project/GameSources/Bubble.h
@@ -3,15 +3,24 @@
 
 namespace basecross
 {
+	enum class BubbleAbility
+	{
+		ShootBubble,
+		RideBubble,
+		TranpolineBubble,
+	};
+
 	class Bubble : public GameObject
 	{
+		unordered_map<BubbleAbility,bool> m_abilities;
 		shared_ptr<Transform> m_trans;
 		shared_ptr<PNTStaticDraw> m_draw;
+		shared_ptr<CollisionSphere> m_col;
 		weak_ptr<GameObject> m_parent;
 		Vec3 m_parentForward;
 		Vec3 m_dir;
+		Vec3 m_scale;
 
-		float m_speed;
 		float m_speedRatio;
 		float m_initialVelocity;
 		float m_currentVelocity;
@@ -20,6 +29,8 @@ namespace basecross
 		float m_limitTime;
 		bool m_isSpawnedTrampoline;
 		bool m_isHit;
+		float m_moveTime;
+		float m_moveTimeLimit;
 
 	public :
 		Bubble::Bubble(const shared_ptr<Stage>& stage,const shared_ptr<GameObject>& parent);
@@ -33,6 +44,22 @@ namespace basecross
 		// ----------------------------------
 		void BubbleMove();
 		
+		bool HasAblity(BubbleAbility ability)
+		{
+			auto it = m_abilities.find(ability);
+
+			if (it == m_abilities.end()) return false;
+			return it->second;
+		}
+
+		void SetAbility(BubbleAbility ability, bool value)
+		{
+			// キーとそのキーがtureかfalseかをセットする
+			m_abilities[ability] = value;
+		}
+
+		void BubbleAddAblity(BubbleAbility ability);
+
 		Vec3 GetCameraForward();
 
 		virtual void OnCollisionEnter(shared_ptr<GameObject>& Other);
