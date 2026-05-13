0a3507f6c626ead74083c22da9779084bc285e7d
Takato Kanno <zeal23420052@fsgcl.jp>
2026-05-13 16:46:55 +0900

    Update：Groundのモデルを変更

--- Project/GameSources/Ground.cpp
+++ Project/GameSources/Ground.cpp
@@ -30,30 +30,18 @@ namespace basecross
 		auto ptrShadow = AddComponent<Shadowmap>();
 		m_sPtrTrans = AddComponent<Transform>();
 
-		ptrDraw->SetMeshResource(L"Stage_Floor");
+		ptrDraw->SetMeshResource(L"DEFAULT_CUBE");
 		ptrDraw->SetTextureResource(L"Ground_Sozai");
 
 		ptrDraw->SetOwnShadowActive(true);
-
-		Mat4x4 spanMat;
-		spanMat.affineTransformation
-		(
-			Vec3(1.0f, 1.0f, 1.0f),
-			Vec3(0.0f, 0.0f, 0.0f),
-			Vec3(0.0f, 0.0f, 0.0f),
-			Vec3(0.0f, -0.5f, 0.0f)
-		);
-
-		ptrShadow->SetMeshResource(L"Stage_Floor");
+		
+		ptrShadow->SetMeshResource(L"DEFAULT_CUBE");
 
 		m_sPtrTrans->SetScale(m_objectParam.GetScale());
 		m_sPtrTrans->SetQuaternion(m_objectParam.GetQuaternion());
 		m_sPtrTrans->SetPosition(m_objectParam.GetPosition());
-
-		ptrDraw->SetMeshToTransformMatrix(spanMat);
-		ptrShadow->SetMeshToTransformMatrix(spanMat);
-
+		
 		auto ptrColl = AddComponent<CollisionObb>();
-		ptrColl->SetFixed(true);
+		ptrColl->SetDrawActive(true);
 	}
 }
