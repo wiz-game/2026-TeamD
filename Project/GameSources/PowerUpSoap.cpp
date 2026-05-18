ce8248a83f7239650a125779eecda66bdb9db8b7
Shin Muraoka <zeal24450012@fsgcl.jp>
2026-05-18 22:13:22 +0900

    add : 触れたときにクールタイムの減少を有効にするようにした

--- Project/GameSources/PowerUpSoap.cpp
+++ Project/GameSources/PowerUpSoap.cpp
@@ -12,6 +12,7 @@ namespace basecross
 	//初期化
 	void PowerUpSoap::OnCreate()
 	{
+		AddTag(L"Soap");
 		m_draw = AddComponent<PNTStaticDraw>();
 		m_draw->SetMeshResource(L"DEFAULT_CUBE");
 
@@ -26,6 +27,23 @@ namespace basecross
 		GetItem(GetThis<PowerUpSoap>());
 	}
 
-	// 当たり判定
+	void PowerUpSoap::GetSoapOfCoolDown()
+	{
+		// 現在有効なステージの情報を取得する
+		auto stage = App::GetApp()->GetScene<Scene>()->GetActiveStage();
+		if (stage == nullptr)	// エラー対策
+		{
+			return;
+		}
+
+		// Playerの情報を取得する
+		auto player = stage->GetSharedGameObject<Player>(L"Player");
+		if (player == nullptr)	// エラー対策
+		{
+			return;
+		}
+
+		player->SetCoolDown(true);
+	}
 }
 //end basecross
