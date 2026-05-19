915bed0c6f08352b3be97e3d855289bb6985054d
Shin Muraoka <zeal24450012@fsgcl.jp>
2026-05-19 20:44:29 +0900

    add : 回転するようにした

--- Project/GameSources/PowerUpSoap.cpp
+++ Project/GameSources/PowerUpSoap.cpp
@@ -15,7 +15,8 @@ namespace basecross
 		AddTag(L"Soap");
 
 		m_transform = AddComponent<Transform>();
-		m_transform->SetScale(0.5f, 1.0f, 0.5f);
+		m_transform->SetScale(m_Scale);
+		m_transform->SetRotation(0.0f, 0.0f, XMConvertToRadians(45.0f));
 
 		m_draw = AddComponent<PNTStaticDraw>();
 		m_draw->SetMeshResource(L"DEFAULT_CUBE");
@@ -28,7 +29,29 @@ namespace basecross
 	// 更新
 	void PowerUpSoap::OnUpdate()
 	{
+		Rotation();
+		DebugStr();
 	}
 
+	void PowerUpSoap::Rotation()
+	{
+		auto transRot = m_transform->GetRotation();
+		transRot.y += m_RotationSpeed * App::GetApp()->GetElapsedTime();
+		m_transform->SetRotation(transRot);
+	}
+
+	void PowerUpSoap::UpDown()
+	{
+		auto transPos = m_transform->GetPosition();
+		transPos.y += App::GetApp()->GetElapsedTime();
+		//m_transform->SetPosition()
+	}
+
+	void PowerUpSoap::DebugStr()
+	{
+		auto transRot = m_transform->GetRotation();
+	
+		GameManager::Instance().AddDebugStr(L"Rotation_Y", transRot.y);
+	}
 }
 //end basecross
