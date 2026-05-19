a2e3958eeeda2c9c3fd296f3ee1488a3f384ca8f
Tomoki Yoshida <zeal23428042@fsgcl.jp>
2026-05-19 13:30:14 +0900

    Add：FirTreeにOnCreateの処理実装

--- Project/GameSources/FirTree.h
+++ Project/GameSources/FirTree.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "stdafx.h"
+
+namespace basecross
+{
+	class FirTree : public GameObject
+	{
+	private:
+		shared_ptr<Transform> m_sPtrTrans;
+	public:
+		FirTree
+		(
+			const shared_ptr<Stage>& StagePtr,
+			const STRUCT_ObjectParam& objectParam
+		);
+		virtual ~FirTree();
+
+		virtual void OnCreate() override;
+	};
+}
