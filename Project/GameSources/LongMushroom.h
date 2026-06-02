1f581ccbeb5bf3114bc4d5e7ae912ccea5022acf
Tomoki Yoshida <zeal23428042@fsgcl.jp>
2026-06-02 15:46:40 +0900

    Add：LongMushroomのOnCreateの中身実装

--- Project/GameSources/LongMushroom.h
+++ Project/GameSources/LongMushroom.h
@@ -1,2 +1,20 @@
 #pragma once
 #include "stdafx.h"
+
+namespace basecross
+{
+	class LongMushroom : public GameObject
+	{
+	private:
+		shared_ptr<Transform> m_sPtrTrans;
+	public:
+		LongMushroom
+		(
+			const shared_ptr<Stage>& StagePtr,
+			const STRUCT_ObjectParam& objectParam
+		);
+		virtual ~LongMushroom();
+
+		virtual void OnCreate() override;
+	};
+}
