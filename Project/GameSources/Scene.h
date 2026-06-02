9c98088016e975b9ebc904d45d6e43e05ce9a22c
Takato Kanno <zeal23420052@fsgcl.jp>
2026-06-02 13:12:10 +0900

    Update：mediaファイルの全てのデータを読み込む（Skipされたもの以外）処理をクラスにまとめた

--- Project/GameSources/Scene.h
+++ Project/GameSources/Scene.h
@@ -11,9 +11,6 @@ namespace basecross
 	class Scene : public SceneBase
 	{
 	private:
-		void RegisterMediaFiles(const wstring& Directory);
-		bool ShouldSkipFolder(const wstring& ParentDir, const wstring& FolderName);
-		void RegisterSingleModel(const wstring& Key, const wstring& ModelDir, const wstring& ModelFile, bool AccessWrite = false);
 	public:
 		Scene() :SceneBase(){}
 		virtual ~Scene();
