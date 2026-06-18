a75089319710307d0c57066495125c46d342debb
Tomoki Yoshida <zeal23428042@fsgcl.jp>
2026-06-18 11:24:23 +0900

    Uppdate：PNTBoneModelDrawに変更した

--- Project/GameSources/EnemyBase.cpp
+++ Project/GameSources/EnemyBase.cpp
@@ -130,7 +130,7 @@ namespace basecross
                     continue;
                 }
 
-                auto drawComp = obj->GetComponent<PNTStaticDraw>(false);
+                auto drawComp = obj->GetComponent<PNTBoneModelDraw>(false);
                 if (drawComp)
                 {
                     Vec3 hitPoint;
@@ -246,7 +246,7 @@ namespace basecross
         auto transComp = gameObject->GetComponent<Transform>();
         auto transPos = transComp->GetPosition();
         auto transRot = transComp->GetRotation();
-        auto drawComp = gameObject->GetComponent<PNTStaticDraw>();
+        auto drawComp = gameObject->GetComponent<PNTBoneModelDraw>();
 
         float forward_X = 0.0f, forward_Z = 0.0f;
         // forward_Xとforward_Zを格納するための変数
@@ -284,7 +284,7 @@ namespace basecross
                 continue;
             }
 
-            auto objDrawComp = obj->GetComponent<PNTStaticDraw>(false);
+            auto objDrawComp = obj->GetComponent<PNTBoneModelDraw>(false);
             if (objDrawComp)
             {
                 Vec3 hitPoint;
