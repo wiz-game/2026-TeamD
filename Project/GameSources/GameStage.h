b8e3e3ee60bd1cab62b2eab2a508692c36b480ad
Fuuki Tamura <zeal24450102@fsgcl.jp>
2026-06-26 13:34:24 +0900

    Update::メニュー

--- Project/GameSources/GameStage.h
+++ Project/GameSources/GameStage.h
@@ -17,6 +17,16 @@ namespace basecross
 		Vec3 m_collisionRange = Vec3(100.0f, 1000.0f, 100.0f);
 		string m_stageNum;
 
+		// メニュー画面のUI
+		vector<shared_ptr<UIBase>> m_uiframes;
+		vector<shared_ptr<UIBase>> m_uidefaults;
+		vector<shared_ptr<UIBase>> m_uisettings;
+		vector<shared_ptr<UIBase>> m_uihowtoplays;
+
+		// メニュー画面のパラメータ
+		Col4 m_uiDiffuse = Col4(1.0f, 1.0f, 1.0f, 1.0f);
+		float m_selectScale = 0.25f;
+
 		void CreateViewLight();
 		void CreatePlayer();
 		void CreateMenu();
