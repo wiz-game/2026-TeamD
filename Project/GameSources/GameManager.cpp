a20cffa6fd26c620e4afa357a6006cdbdd1a68eb
Takato Kanno <zeal23420052@fsgcl.jp>
2026-06-30 14:19:22 +0900

    BugFix：クリアできない場合があったのを修正

--- Project/GameSources/GameManager.cpp
+++ Project/GameSources/GameManager.cpp
@@ -62,6 +62,7 @@ namespace basecross
 		case ENUM_GameMode::GameOver:
 			break;
 		case ENUM_GameMode::Play:
+			m_dirtNum = 0;
 			break;
 		case ENUM_GameMode::Menu:
 			MenuManager::Instance().ClosePause();
