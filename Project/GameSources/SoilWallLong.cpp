35a8f3f3e983ba12058ffd9fde01d06e6c98a344
Fuuki Tamura <zeal24450102@fsgcl.jp>
2026-07-07 14:23:10 +0900

    Add::ファイルの追加

---
+++ Project/GameSources/SoilWallLong.cpp
@@ -0,0 +1,12 @@
+/*!
+@file SoilWall.cpp
+@brief 土の壁(Long)
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross
+{
+
+}
---
+++ Project/GameSources/UIStatueNum.cpp
@@ -0,0 +1,12 @@
+/*!
+@file SoilWall.cpp
+@brief 綺麗にしたあわパカ像
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross
+{
+
+}
