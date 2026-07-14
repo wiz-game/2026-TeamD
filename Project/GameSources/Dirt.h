832914f7523a20c70fd48d02a91a38ceb512eb25
Takato Kanno <zeal23420052@fsgcl.jp>
2026-07-14 09:43:01 +0900

    コンフリクト回避

--- Project/GameSources/Dirt.h
+++ Project/GameSources/Dirt.h
@@ -35,7 +35,7 @@ namespace basecross
 		~Dirt();
 
 		virtual void OnCreate()override;
-		virtual void OnDestroy()override;
+		virtual void OnUpdate()override;
 
 		void SetDirtHP(const float HP);
 		float GetDirtHP() const { return m_HP; }
