609709150cb0dcce9a3b11fdc4f74b9adea8bb29
Tomoki Yoshida <zeal23428042@fsgcl.jp>
2026-07-15 10:51:14 +0900

    Update：UIを変更した

--- Project/GameSources/GameOverStage.cpp
+++ Project/GameSources/GameOverStage.cpp
@@ -14,9 +14,9 @@ namespace basecross
 		try
 		{
 			CreateView<SingleView>();
-			AddGameObject<UIBase>(STRUCT_UIParam(L"GameOver", Vec3(0.0f, 140.0f, 0.0f), 0.7f));
-			AddGameObject<UIBase>(STRUCT_UIParam(L"RetrunA", Vec3(0.0f, -140.0f, 0.0f), 0.2f));
-			AddGameObject<UIBase>(STRUCT_UIParam(L"TitleModoru", Vec3(0.0f, -260.0f, 0.0f), 0.2f));
+			AddGameObject<UIBase>(STRUCT_UIParam(L"UI_GameOver", Vec3(0.0f, 140.0f, 0.0f), 0.7f));
+			AddGameObject<UIBase>(STRUCT_UIParam(L"UI_OptionButton_5", Vec3(0.0f, -140.0f, 0.0f), 0.25f));
+			AddGameObject<UIBase>(STRUCT_UIParam(L"UI_OptionButton_4", Vec3(0.0f, -260.0f, 0.0f), 0.25f));
 
 		}
 		catch (...)
